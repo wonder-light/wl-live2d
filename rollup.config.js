1bc63f7dfebcb0d1f63fb9dd10925e1f5f99ca4a
念 <3209752489@qq.com>
2024-03-10 18:32:38 +0800

    修改rollup配置

--- rollup.config.js
+++ rollup.config.js
@@ -6,51 +6,68 @@ import terser from '@rollup/plugin-terser';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import nested from 'postcss-nested';
+import copy from 'rollup-plugin-copy';
 import polyfill from 'rollup-plugin-polyfill-node';
 import postcss from 'rollup-plugin-postcss';
 
-const input = [
-  'lib/index.js',
-  'lib/index.cubism2.js',
-  'lib/index.cubism4.js'
-];
-const plugins = [
-  polyfill(),
-  resolve({
-    browser: true,
-    preferBuiltins: false
-  }),
-  commonjs(),
-  json(),
-  postcss({
+
+const config = (input) => {
+  return {
+    input: input,
+    output: [
+      {
+        dir: 'dist/es',
+        format: 'es',
+        preserveModules: false // 保留模块结构
+      },
+      {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        exports: 'named'
+      },
+      {
+        dir: 'dist/umd',
+        format: 'umd',
+        name: 'wl-live2d',
+        exports: 'named'
+      }
+    ],
+    context: 'windows',
+    moduleContext: 'windows',
+    external: [
+      /packages/
+    ],
     plugins: [
-      autoprefixer(),
-      nested(),
-      cssnano()
+      copy({
+        targets: [
+          { src: 'packages', dest: 'dist' }
+        ]
+      }),
+      babel({
+        babelHelpers: 'bundled'
+      }),
+      resolve({
+        browser: true,
+        preferBuiltins: true
+      }),
+      polyfill(),
+      commonjs(),
+      json(),
+      postcss({
+        plugins: [
+          autoprefixer(),
+          nested(),
+          cssnano()
+        ]
+      }),
+      terser()
     ]
-  }),
-  babel({
-    babelHelpers: 'bundled'
-  }),
-  terser()
-];
+  };
+};
 
 export default [
-  {
-    input,
-    output: {
-      dir: 'dist/es',
-      format: 'es'
-    },
-    plugins
-  },
-  {
-    input,
-    output: {
-      dir: 'dist/cjs',
-      format: 'cjs'
-    },
-    exports: 'named',
-    plugins
-  }
+  config('lib/index.js'),
+  config('lib/index.cubism2.js'),
+  config('lib/index.cubism4.js'),
 ];
+
