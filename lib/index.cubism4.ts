7d001cb447bdbc3fd87206c564789557f9b1f687
念 <3209752489@qq.com>
2025-08-16 07:59:13 +0800

    更新 index.ts 文件中属性的排序位置

--- lib/index.cubism4.ts
+++ lib/index.cubism4.ts
@@ -5,11 +5,10 @@ import { ULive2dController } from './controller';
 import type { TLive2dCreate } from './types';
 import './css/index.css';
 
-window.PIXI = PIXI;
-window.ILive2DModel = Live2DModel;
-
 const wlLive2d: TLive2dCreate = ULive2dController.create;
+window.PIXI = PIXI;
 window.wlLive2d = wlLive2d;
+window.ILive2DModel = Live2DModel;
 
 export default wlLive2d;
 export { wlLive2d };
--- lib/index.ts
+++ lib/index.ts
@@ -6,12 +6,10 @@ import { ULive2dController } from './controller';
 import type { TLive2dCreate } from './types';
 import './css/index.css';
 
-// PIXI.settings.RENDER_OPTIONS.hello = false; 7.0.0 以上版本使用
-window.PIXI = PIXI;
-window.ILive2DModel = Live2DModel;
-
 const wlLive2d: TLive2dCreate = ULive2dController.create;
+window.PIXI = PIXI;
 window.wlLive2d = wlLive2d;
+window.ILive2DModel = Live2DModel;
 
 export default wlLive2d;
 export { wlLive2d };
